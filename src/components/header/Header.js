74d18d2fc1ac7696c63034c9fca192d94cd96f85
mrsamirkhan <sirsamir@outlook.com>
2022-03-23 15:12:47 +0600

    adding react project

---
+++ src/components/header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from '../../images/Logo.svg';
+import './Header.css';
+const Header = () => {
+    return (
+        <nav className="header-nav">
+            <a href="/order"><img src={logo} alt="" /></a>
+            <div>
+                <a href="/order">Order</a>
+                <a href="/order-rivew">Order Rivew</a>
+                <a href="/manage-history">Manage History</a>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
---
+++ src/components/product/Product.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping} from '@fortawesome/free-solid-svg-icons';
+import './Product.css';
+
+const Product = (props) => {
+    // const {addToCart, product} = props;
+    const {img, name, ratings, seller} = props.product; 
+
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+            <h6 className='product-name'>{name}</h6>
+            <p>Seller:{seller}</p>
+            <p>Ratings:{ratings}</p>
+            <button onClick={()=>props.AddtoCart(props.product)} className='button'>Add To Cart
+            <p><FontAwesomeIcon icon={faCartShopping}></FontAwesomeIcon></p>
+            </button>
+        </div>
+    );
+};
+
+export default Product;
